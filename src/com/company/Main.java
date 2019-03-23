c47ca20981fa91448594f1c3f5c7b39536c58ad1
Unknown <ruben.dominguez@opendeusto.es>
2019-03-23 15:02:04 +0100

    Prueba de edicion PCPortatil

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
 	// Comentario 12/03 a las 14:32 en PC Sobremesa (Rubens)
         System.out.println("._.");
+
+        System.out.println("Desde el portatil");
         clsMenu.MenuPrincipal();
     }
 }
