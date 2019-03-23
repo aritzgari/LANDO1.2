c71c86bcf5a17d6c9df84b2a15da420cc660e05d
Rubén <ruben.dominguez@opendeusto.es>
2019-03-23 14:34:06 +0100

    Revert "Prueba de edición PCSobremesa"
    This reverts commit 5ddaf21193fa096c89dff662da72a2b5dcacaab8.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ import com.company.LP.clsMenu;
 public class Main {
 
     public static void main(String[] args) {
-	// Comentario 12/03 a las 14:32 en PC Sobremesa (Rubens)
-        System.out.println("._.");
+	// write your code here
         clsMenu.MenuPrincipal();
     }
 }
